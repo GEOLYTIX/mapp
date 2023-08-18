31f134db58221d58c4dbea3727e21c5b88d42df0
Dennis Bauszus <dennis.bauszus@geolytix.co.uk>
2023-08-18 20:45:20 +0100

    Update sentiment_theme.js

--- plugins/sentiment_theme.js
+++ plugins/sentiment_theme.js
@@ -71,8 +71,6 @@ export default (function () {
             }
         }
 
-        // memoizedStyles[feature.properties.sentiment] = style;
-
         feature.style = style;
 
         console.log(renderCount++)
